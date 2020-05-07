9926afe4d5b2f448ed03b1af10a21622e8753e32
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-07 16:15:23 +0600

    check vowel

---
+++ Day13/check_vowel.dart
@@ -0,0 +1,16 @@
+
+/*
+Hello !! I Love Dart Programming Language
+Author:Mehedi Hasan
+Topics: Problem Solving ||Algorithm & Data Structure 
+Problem Name: Check vowel using if else
+*/
+
+void main(){
+  String s = 'i';
+  if(s == 'a' || s == 'A' || s == 'e' || s == 'E' || s == 'i' || s == 'I' || s == 'o' || s == 'O' || s == 'u' || s == 'U'){
+    print("$s is Vowel");
+  }else{
+    print("$s is Consonent");
+  }
+}
