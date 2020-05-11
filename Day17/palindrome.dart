3bd795efc901ec7f164af7d4156cce9727e1f90a
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-11 11:12:22 +0600

    palindrome number

--- Day17/palindrome.dart
+++ Day17/palindrome.dart
@@ -6,14 +6,21 @@ Problem Name: Dart program for palindrome number
 */
 
 void main(){
-  int inputNumber = 12345;
+  int inputNumber = 123321;
   int reminder, sum = 0;
+  int temp = inputNumber;
 
   while(inputNumber !=  0){
-    reminder = inputNumber % 10; // when 12345 % 10 then reminder is 5, agin 1234 % 10 reminder is 4, continue
-    sum = sum * 10 + reminder; //0*10 + 5 = 5; agin 5*10 + 4 =54 be continue.....
-    inputNumber = inputNumber ~/ 10; //12345/10 = 1234.5 = 1234
+    reminder = inputNumber % 10;  // when 12345 % 10 then reminder is 5, agin 1234 % 10 reminder is 4, continue
+     sum = sum * 10 + reminder;  //0*10 + 5 = 5; agin 5*10 + 4 =54 be continue.....
+    inputNumber = inputNumber ~/ 10;  //12345/10 = 1234.5 = 1234
   }
 
-  print("Reverse number is : $sum");
+  inputNumber = temp;
+  
+  if(inputNumber == sum){
+    print("$inputNumber is palindrome number");
+  }else{
+    print("$inputNumber is not palindrome number");
+  }
 }
