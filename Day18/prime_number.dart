b91623e42b670d1d42912e93a0c14102a153b062
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-12 13:04:50 +0600

    prime number

---
+++ Day18/prime_number.dart
@@ -0,0 +1,21 @@
+/*
+Hello !! I Love Dart Programming Language
+Author: Mehedi Hasan
+Topics: Problem Solving || Algorithm & Data Structure 
+Problem Name: Prime number in Dart language
+*/
+
+void main() {
+  int i, count = 0, inputNumber = 8;
+  for (i = 2; i < inputNumber; i++) {
+    if (inputNumber % i == 0) { 
+      count++;
+      break;
+    }
+  }
+  if(count == 0){
+    print("$inputNumber is Prime Number");
+  }else{
+    print("$inputNumber is not prime number");
+  }
+}
