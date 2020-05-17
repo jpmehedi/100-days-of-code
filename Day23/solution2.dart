69807621d10f5baf135258cdec1be83f13992f84
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-17 16:11:03 +0600

    solution2 day23

---
+++ Day23/solution2.dart
@@ -0,0 +1,20 @@
+/*
+Hello !! I Love Dart Programming Language
+Author: Mehedi Hasan
+Topics: Problem Solving || Algorithm & Data Structure 
+Problem Name:Write a C program to check whether a given temperatures is 
+less than 0 and the other is greater than 100.
+*/
+
+void main(){
+
+print(tempCheck(-1, 120));
+print(tempCheck(125, -1));
+print(tempCheck(2, 130));
+
+
+}
+
+tempCheck(int temp1, int temp2){
+  return temp1 < 0 && temp2 > 100 || temp2 < 0 || temp1 > 100;
+}
