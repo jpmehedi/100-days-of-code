544f7e110d0dc8775bb216973b2a31764c7f24a7
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-05-24 17:11:00 +0600

    solution3 way1

---
+++ Day30/solution3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int i,count=0;
+   for(i=1000;i>=1;i--)
+   {
+       printf("%d\t",i);
+       i=i-1;
+       printf("%d\t",i);
+       i=i-1;
+      printf("%d\t",i);
+       i=i-1;
+        printf("%d\t",i);
+       i=i-1;
+        printf("%d\t",i);
+      printf("\n");
+   }
+
+ return 0;
+}
